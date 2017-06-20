b1b93170fc0c510cd45ba85f97aee07099bee5c9
Mark Manley <mark.manley.2010@gmail.com>
2017-06-19 20:52:52 -0400

    initial commit

--- rollup.config.js
+++ rollup.config.js
@@ -1,34 +1,34 @@
-import babel from 'rollup-plugin-babel';
-import babelrc from 'babelrc-rollup';
-import istanbul from 'rollup-plugin-istanbul';
+import babel from "rollup-plugin-babel";
+import babelrc from "babelrc-rollup";
+import istanbul from "rollup-plugin-istanbul";
 
-let pkg = require('./package.json');
+let pkg = require("./package.json");
 let external = Object.keys(pkg.dependencies);
 
-let plugins = [
-  babel(babelrc()),
-];
+let plugins = [babel(babelrc())];
 
-if (process.env.BUILD !== 'production') {
-  plugins.push(istanbul({
-    exclude: ['test/**/*', 'node_modules/**/*']
-  }));
+if (process.env.BUILD !== "production") {
+  plugins.push(
+    istanbul({
+      exclude: ["test/**/*", "node_modules/**/*"]
+    })
+  );
 }
 
 export default {
-  entry: 'lib/index.js',
+  entry: "lib/index.js",
   plugins: plugins,
   external: external,
   targets: [
     {
       dest: pkg.main,
-      format: 'umd',
-      moduleName: 'rollupStarterProject',
+      format: "umd",
+      moduleName: "functionX",
       sourceMap: true
     },
     {
       dest: pkg.module,
-      format: 'es',
+      format: "es",
       sourceMap: true
     }
   ]
--- test/index_test.js
+++ test/index_test.js
@@ -1,28 +1,44 @@
-import { multiply } from '../';
-import { strictEqual } from 'assert';
+require("mocha-testcheck").install();
+const { expect } = require("chai");
 
-describe('multiply', () => {
-  it('returns 0 when either argument is 0', () => {
-    strictEqual(multiply(0, 2), 0);
-    strictEqual(multiply(4, 0), 0);
+import { add } from "../lib/utils";
+import { multiply } from "../";
+import { strictEqual } from "assert";
+
+describe("add", () => {
+  check.it("is commutative", gen.int, gen.int, (A, B) => {
+    expect(add(A, B)).to.equal(add(B, A));
+  });
+});
+
+describe("multiply", () => {
+  check.it("is commutative", gen.int, gen.int, (A, B) => {
+    expect(multiply(A, B)).to.equal(multiply(B, A));
   });
 
-  it('returns the value of one number if the other is 1', () => {
-    strictEqual(multiply(1, 8), 8);
-    strictEqual(multiply(5, 1), 5);
+  check.it("returns 0 when either argument is 0", gen.int, gen.int, (A, B) => {
+    expect(multiply(A, 0)).to.equal(0);
+    expect(multiply(0, B)).to.equal(0);
   });
 
-  it('is commutative', () => {
-    strictEqual(multiply(2, 4), multiply(4, 2));
+  check.it(
+    "returns the value of one number if the other is 1",
+    gen.int,
+    gen.int,
+    (A, B) => {
+      expect(multiply(A, 1)).to.equal(A);
+      expect(multiply(1, B)).to.equal(B);
+    }
+  );
+
+  check.it("is commutative", gen.int, gen.int, (A, B) => {
+    expect(multiply(A, B)).to.equal(multiply(B, A));
   });
 
-  it('returns the product of the two numbers', () => {
-    strictEqual(multiply(11, 9), 99);
-  });
-
-  it('handles negative numbers', () => {
-    strictEqual(multiply(-2, 2), -4);
-    strictEqual(multiply(2, -2), -4);
-    strictEqual(multiply(-2, -2), 4);
+  check.it("handles negative numbers", gen.sNegInt, gen.sPosInt, (N, P) => {
+    expect(multiply(N, P)).to.be.lessThan(0);
+    expect(multiply(P, N)).to.be.lessThan(0);
+    expect(multiply(N, N)).to.be.greaterThan(0);
+    expect(multiply(P, P)).to.be.greaterThan(0);
   });
 });
